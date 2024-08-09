e0efa5953e805515526ed02d4ca0f4701757563a
luisalbeto <luis-alberto11@hotmail.com>
2024-08-09 11:07:27 -0400

    Feat: Update Todo

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,9 +1,30 @@
 
+import prisma from "@/lib/prisma"
+import { TodosGrid } from "@/todos"
+import { useEffect } from "react"
 
-export default function ResTodosPage(){
+
+export const metadata = {
+  title: 'Listado de Todos',
+  description: 'SEO Title'
+}
+
+
+export default async function ResTodosPage(){
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+
+ // useEffect(() => {
+   // fetch('/api/todos')
+    //.then( resp => resp.json())
+   // .then(console.log)
+  //},[])
+
+
   return(
   <div>
-    <h1>Page RestTodos</h1>
+    {/* TODO: Formulario para agregar todo */}
+    <TodosGrid todos={ todos }/>
   </div>
   )
 }
