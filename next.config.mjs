b32940c0b5e8f66b9a281ca9f490c2363e3f5e4f
luisalbeto <luis-alberto11@hotmail.com>
2024-08-14 15:32:24 -0400

    Feat: Auth provider with github & google

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,19 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'tailus.io'
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com'
             }
         ]
     }
 };
 
 export default nextConfig;
+
+
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,9 @@ import Image from "next/image"
 import Link from "next/link"
 import { CiLogout } from "react-icons/ci"
 import { SidebarItem } from "./SidebarItem"
-import { IoBasketOutline, IoCalendarClearOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from "react-icons/io5"
+import { IoBasketOutline, IoCalendarClearOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPersonOutline } from "react-icons/io5"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { getServerSession } from "next-auth"
 
 
 const menuItems = [
@@ -31,10 +33,25 @@ const menuItems = [
     title: 'Products',
     path: '/dashboard/products'
   },
+  {
+    icon: <IoPersonOutline/>,
+    title: 'Perfil',
+    path: '/dashboard/profile'
+  },
 ]
 
 
-export const Sidebar = () => {
+export const Sidebar = async() => {
+
+  const session = await getServerSession(authOptions)
+
+  const avatarUrl = ( session?.user?.image)
+  ? session.user.image
+  : 'https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg'
+
+  const userName = session?.user?.name ?? 'No Name'
+  // TODO: const userRole = session?.user?.name ?? 'No Name'
+
   return(
     <>
       <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -55,13 +72,13 @@ export const Sidebar = () => {
           <div className="mt-8 text-center">
             {/* Next/Image */}
             <Image 
-              src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" 
+              src={avatarUrl} 
               alt="" 
               className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" 
               width={150} 
               height={150}/>
 
-              <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+              <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{userName}</h5>
               <span className="hidden text-gray-400 lg:block">Admin</span>
           </div>
 
